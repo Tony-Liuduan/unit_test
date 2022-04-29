1929c366b08b666c4ee7fd9e132260a6d4a9e4d3
liuduan <liuduan@fenbi.com>
2022-04-29 19:49:18 +0800

    MOD: update mocharunner

--- mochaRunner.js
+++ mochaRunner.js
@@ -1,19 +1,23 @@
 const Mocha = require('mocha');
+const moment = require('moment');
+const path = require('path');
+const { exec } = require('child_process');
 
+const timestamp = moment().format('YYYY-MM-DD_HH:mm:ss');
+const reportDirName = 'mochawesome-report-' + timestamp;
+const reportDirPath = path.resolve(__dirname, 'docs', reportDirName);
 
 const mocha = new Mocha({
-    reporter: 'mochawesome',
-    reporterOptions: {
-        reportDir: "./docs/mochawesome-report",
-    },
+	reporter: 'mochawesome',
+	reporterOptions: {
+		reportDir: reportDirPath,
+		reportFilename: 'index',
+	},
 });
-
 
 mocha.addFile('./tests/service/router.spec.js');
 
-
 mocha.run(function () {
-    console.log('all done');
-    process.exit();
-})
-
+	console.log('all done');
+	process.exit(url);
+});
